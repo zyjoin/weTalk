b5ce7a903954835eaa012aee16bcb8bd3ffe64f2
庄远 <zhuangyuan@promote.cache-dns.local>
2016-03-28 09:54:50 +0800

    添加一个Person类

---
+++ weTalk/weTalk/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  weTalk
+//
+//  Created by 庄远 on 16/3/28.
+//  Copyright © 2016年 Zy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Person : UIView
+
+@end
---
+++ weTalk/weTalk/Person.m
@@ -0,0 +1,21 @@
+//
+//  Person.m
+//  weTalk
+//
+//  Created by 庄远 on 16/3/28.
+//  Copyright © 2016年 Zy. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
